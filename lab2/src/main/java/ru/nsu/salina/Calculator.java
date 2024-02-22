025ba4dbcc86d2c25d6781a47b859d2aec806ef3
diana-salina <d.salina@g.nsu.ru>
2024-02-22 17:33:11 +0700

    lab1 added

--- lab2/src/main/java/ru/nsu/salina/Calculator.java
+++ lab2/src/main/java/ru/nsu/salina/Calculator.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Calculator {
-    private Stack<String> TaskStack;
+    private Stack<String> TaskStack = new Stack<String>();
+    public Stack<Integer> ExecuteStack = new Stack<Integer>();
 
     public void DoTask(Scanner scanner) {
         TaskStack = InputReader.getStack(scanner);
         for (String operator : TaskStack) {
             String[] splited = InputReader.SplitStackElement(operator);
             Command currentCommand = CommandFactory.findCommand(splited[0]);
-            currentCommand.executeCommand();
+            currentCommand.executeCommand(splited);
         }
     }
 }
--- lab2/src/main/java/ru/nsu/salina/Command.java
+++ lab2/src/main/java/ru/nsu/salina/Command.java
@@ -3,40 +3,40 @@ package ru.nsu.salina;
 import javax.print.DocFlavor;
 
 public interface Command {
-    public void executeCommand();
+    public void executeCommand(String[] splited);
 }
 
 class CommentCommand implements Command {
     @Override
-    public void executeCommand() {
+    public void executeCommand(String[] splited) {
 
     };
 }
 
 class DefineCommand implements Command {
     @Override
-    public void executeCommand() {
+    public void executeCommand(String[] splited) {
 
     }
 }
 
 class PopCommand implements Command {
     @Override
-    public void executeCommand() {
+    public void executeCommand(String[] splited) {
 
     }
 }
 
 class PrintCommand implements Command {
     @Override
-    public void executeCommand() {
+    public void executeCommand(String[] splited) {
 
     }
 }
 
 class PuchCommand implements Command {
     @Override
-    public void executeCommand() {
+    public void executeCommand(String[] splited) {
 
     }
 }
